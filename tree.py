3f234cc1c93436e54574ab8e70d23c8beefc3f3e
Iskander <alimov.ikh@phystech.edu>
2021-12-28 11:10:55 +0300

    realize tree

--- tree.py
+++ tree.py
@@ -1,5 +1,37 @@
+class Node:
+    def __init__(self):
+         self.right = None
+         self.left =  None
+         self.value = None
+
+    def add(self, val):
+        if self.value is None:
+            self.value = val
+        elif self.value >= val:
+            if self.right is None:
+                self.right = Node()
+            self.right.add(val)
+        else:
+            if self.left is None:
+                self.left = Node()
+            self.left.add(val)
+    def printTree(self):
+        if self.right is not None:
+            self.right.printTree()
+        print(self.value)
+        if self.left is not None:
+            self.left.printTree()
+
+
+
 def main():
-    pass
+    tree = Node()
+    tree.add(1)
+    tree.add(10)
+    tree.add(20)
+    tree.add(2)
+    tree.add(5)
+    tree.printTree()
 
 
 if __name__ == '__main__':
